4de71b4c027965cae5953bcca17e8d4671668000
Sindhu202 <sindhuca121@gmail.com>
2023-04-29 13:37:52 +0530

    first push to git

---
+++ python/mul_table.py
@@ -0,0 +1,10 @@
+m=int(input("enter range one "))
+n=int(input("enter range two "))
+
+
+for m in range(n+1):
+    for i in range(11):
+        x=m*i
+        y="{}*{}={}".format(m,i,x)
+        print(y-=)
+    
